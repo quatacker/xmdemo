314da11b1cf526ccbf1f0d5ae3dce39f120de8e3
Dane Quatacker <dquatacker@ps-mbp-dquatacker.local>
2017-05-08 14:21:12 -0700

    Fixed environment file for port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require("./app.js")
 
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
