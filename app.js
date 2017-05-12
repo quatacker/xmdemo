0cc7f44a6e44e67a1f70080738665222780ea983
Dane Quatacker <dquatacker@ps-mbp-dquatacker.local>
2017-05-12 13:17:11 -0700

    made app generic fixed vscode debugging

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('I LOVE xmatters!')
+  res.send('web page message')
 })
 
 module.exports = app;
